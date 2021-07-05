af67f74977c625d74e0eee72f7bdf84292616145
kirangautam45 <gautamkiran38@gmail.com>
2021-07-05 12:51:06 +0545

    registration

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const bodyParser=require('body-parser');
 const cors =require('cors');
 const bd=require('./database/db');
 
+const user_route = require('./routes/user_route');
+
 const app = express();
 
 app.use(express.json());
-app.use(core())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(90);
+app.use(user_route);
+
+app.listen(90, () => {
+    console.log("This server is running at port : 90")
+})

8328a2123dc76050f693fd640a27609701666424
RameshBhandari1 <ramesh152200@gmail.com>
2021-07-05 17:55:35 +0545

    Correction in Database name

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/servNep',{
+mongoose.connect('mongodb://127.0.0.1:27017/serveNep',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology : true

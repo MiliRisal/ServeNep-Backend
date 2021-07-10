3204519d5640b1c5a65370bdd8bcaf582365567b
kirangautam45 <gautamkiran38@gmail.com>
2021-07-10 11:43:40 +0545

    error in test

---
+++ tests/user_route.test.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const request = require('supertest');
+
+const user_route = require('../routes/user_route');
+require('./setup');
+
+const app = express();
+app.use(express.json());
+app.use("/", user_route);
+
+describe('Test for user route', () => {
+    test('should be able to register a user', () => {
+        return request(app).post('/user/insert')
+        .send({
+            "fullName":"kiran Gautam",
+            "phone": "980898989",
+            "address": "Maitidevi, Kathmandu",
+            "email": "gautamkiran38@gmail.com",
+             "role": "Customer",
+             "password": "password"
+        })
+        .then(res => {
+            expect(res.statusCode).toBe(201);
+        });
+    });
+
+    test('should not be able to register a user without a phone no', () => {
+        return request(app).post('/user/insert')
+        .send({
+            "fullName":"kiran Gautam",
+            "address": "Maitidevi, Kathmandu",
+            "email": "gautamkiran38@gmail.com",
+             "role": "Customer",
+             "password": "password"
+        })
+        .then(res => {
+            expect(res.statusCode).toBe(400);
+        });
+    });
+
+    
+});

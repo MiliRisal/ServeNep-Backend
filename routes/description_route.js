f8e006eeeedab13404ef75305d3138c704eb4eb9
kirangautam45 <gautamkiran38@gmail.com>
2021-07-12 17:30:54 +0545

    task description_id update get all and one and delete

--- routes/description_route.js
+++ routes/description_route.js
@@ -23,4 +23,52 @@ router.post('/description/insert', function(req, res){
     });
 });
 
+router.get('/description/all', function(req, res){
+    description.find().then(function(data){
+        res.status(200).json({data});
+    })
+    .catch(function(error){
+        res.status(500).json({error:error});
+   });
+});
+
+router.get('/description/:description_id:', function(req, res){
+    const id = req.params.description_id;
+    description.findOne({_id:id}).then(function(result){
+        res.status(200).json(result);
+    })
+    .catch(function(error){
+        res.status(500).json({error:error});
+    });
+});
+router.put('/description/update/:description_id', function(req, res){
+    var title = req.body.title;
+    var taskDescription= req.body.taskDescription;
+    var  estimatedTime = req.body.estimatedTime;
+    var price = req.body.price;
+    const id=req.params.description_id;
+
+    description.findOne({_id:id},{
+        title: title,
+        taskDescription: taskDescription,
+        estimatedTime: estimatedTime,
+        price: price,
+    })
+    .then(function(result) {
+        res.status(200).json({success:true,message: " description Update Success"});
+    })
+    .catch(function(error){
+        res.status(500).json({error:error});
+    });
+
+});
+
+router.delete("/Product/delete/:description_id", function(req,res){
+    const id =req.params.description_id;
+    description.deleteOne({_id:id}).then(function(result){
+    res.status(200).json({success:true, message:"description Delete Success"});
+}).catch(function (error){
+    res.status(500).json({error:error});
+});
+});
 module.exports=router;
